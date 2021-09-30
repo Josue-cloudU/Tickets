3322bc4ee92c87abf2b8c0f4fac9d77213af6971
Josue-cloudU <75402445+Josue-cloudU@users.noreply.github.com>
2021-09-30 16:54:26 -0700

    heroku

--- Tickets/settings.py
+++ Tickets/settings.py
@@ -138,3 +138,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_on_heroku.settings(locals())
